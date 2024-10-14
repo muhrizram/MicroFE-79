3074187dc484c1cabb6c347de283f3003e393cd6
MRizkiRamadhan <muhrizram19@gmail.com>
2024-10-14 16:57:16 +0700

    init

---
+++ login/next.config.mjs
@@ -0,0 +1,24 @@
+import NextFederationPlugin from '@module-federation/nextjs-mf';
+
+const nextConfig = {
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'login',
+        remotes: {},
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {},
+        extraOptions: {
+          debug: false, // `false` by default
+          exposePages: false, // `false` by default
+        },
+        shared: {},
+      })
+    );
+
+    return config;
+  },
+};
+
+export default nextConfig;
---
+++ login/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="h-[100vh] flex justify-center items-center">
+      Hello login app
+    </main>
+  );
+}
